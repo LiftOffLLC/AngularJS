4387ad15f07782eb992f639e22308ed18d8fbeda
kmsagar <harsha@liftoffllc.com>
2014-06-02 20:01:47 +0530

    cgift - single page app

--- cgiftAngularJs/cgiftLogic.rb
+++ cgiftAngularJs/cgiftLogic.rb
@@ -1,27 +1,79 @@
 require 'sinatra'
 require 'net/http'
 require 'json'
+require 'haml'
 
+# enabling the session for the app
 enable :sessions
 
+@@flglogout||=false
+
+# main route where the application starts 
 get '/cgift' do 
+	
+	# message in-case of logout
+	@flglogout_local= @@flglogout
+	@@flglogout=false
+
 	# checking for session
 	session["session_mail_id"]||=nil
 	if session["session_mail_id"] != nil
-		erb :homePage
+		haml :homePage
 	else
-		erb :firstPage
+		haml :firstPage
 	end
-	# @flg_out||=false
-	# @flg_out=params[:flglogout]
 end
 
-# route handling login of a user
+# route for login page
+get '/loginPageView' do 
+	
+	# checking for session
+	session["session_mail_id"]||=nil
+	if session["session_mail_id"] != nil
+		haml :homePage
+	else
+		haml :loginPage 
+	end
+end
+
+# route for home page after authentication
+get '/homePageView' do 
+	
+	# checking for session
+	session["session_mail_id"]||=nil
+	if session["session_mail_id"] != nil
+		haml :homePage
+	else
+		haml :firstPage
+	end
+end
+
+# route for registration page for the new users
+get '/registrationPageView' do 
+	
+	# checking for session
+	session["session_mail_id"]||=nil
+	if session["session_mail_id"] != nil
+		haml :homePage
+	else
+		haml :registration
+	end
+end
+
+get '/logout' do 
+	session["session_mail_id"]=nil
+	@@flglogout=true	
+	redirect("/cgift")
+end
+
+# route handling the login scenario
 post '/login' do 
+	
 	# extracting login user credentials
 	usr_details = JSON.parse(request.body.read)  
 	email = usr_details["mail_id"]
 	@password = usr_details["password"]
+	
 	# preparing request
 	url = "http://bloom-api.herokuapp.com/v1/users/email/#{email}/authenticate"
 	uri = URI.parse(url)
@@ -32,40 +84,28 @@ post '/login' do
     http.use_ssl = (uri.port == 443 or uri.port == 4443)
     response =  http.request(request)
     body = response.body
+	
 	# validation scenario
 	loginStatusMsg=nil
-	if body.nil?
+    if !(response.kind_of? Net::HTTPSuccess)
 		loginStatusMsg="credentials do not match, please enter valid email-id & password" 
 	else
-		# setting session 
+		# starting session 
 		session["session_mail_id"]=email
 	end
     return {"msg"=>loginStatusMsg}.to_json
 end
 
-get '/home' do 
-	# checking for session
-	session["session_mail_id"]||=nil
-	if session["session_mail_id"] != nil
-		erb :homePage
-	else
-		redirect("/cgift")
-	end
-end
-
-get '/logout' do 
-	session["session_mail_id"]=nil
-	redirect("/cgift")
-end
-
 # route handling registration of users 
 post '/register' do 
+
 	# extracting registering user credentails
 	usr_details = JSON.parse(request.body.read)  
 	@first_name = usr_details["first_name"]
 	@last_name = usr_details["last_name"]
 	@mail_id = usr_details["mail_id"]
 	@password = usr_details["password"]
+
 	# preparing request 
 	url = 'http://bloom-api.herokuapp.com/v1/users'
 	uri = URI.parse(url)
@@ -74,21 +114,24 @@ post '/register' do
 	request.body = {"first_name"=> @first_name, "last_name"=> @last_name, "email"=> @mail_id, "password"=> @password}.to_json
 	http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.port == 443 or uri.port == 4443)
+
     # getting response object
 	response =  http.request(request)
     body = response.body
     halt(400, "Something is wrong") if body.nil?
-	    # getting response in terms of hash, for ruby
+
+    # getting response in terms of hash, for ruby
     res_hash = JSON.parse(body)
+
 	# checking for newly registering user
     if res_hash.has_key?("id")
-	    # setting session value  
+		# starting session   
 		session["session_mail_id"]=@mail_id	    
 		return {"msg"=>"The registration is successful, kindly login using credentials"}.to_json
 	end
+
 	# validation scenario
     if res_hash.has_key?("error")
-
 		session["session_mail_id"]=nil
 	    return {"msg"=>"This email is already registered, kindly login"}.to_json
 	end
--- cgiftAngularJs/public/fPage.js
+++ cgiftAngularJs/public/fPage.js
@@ -1,52 +1,80 @@
-//firstPageApp definition
-var firstPage = angular.module('firstPageApp', ['ngResource']);
+//binding the application with angularjs & injecting the dependencies
+var firstPage = angular.module('firstPageApp', ['ngResource', 'ngRoute']);
+
+firstPage.config(['$routeProvider', function($routeProvider){
+  // configuring multiple pages with corresponding URLs, reflecting single page applications   
+  $routeProvider
+    .when('/', {templateUrl: '/loginPageView', controller: 'mainCtrl'})
+    .when('/home', {templateUrl: '/homePageView', controller: 'homeCtrl'})    
+    .when('/register', {templateUrl: 'registrationPageView',   controller: 'RegFormCtrl'})
+    .otherwise({redirectTo: '/'});
+}]);
 
 //login form controller
-function LoginFormCtrl($scope,$resource) 
-{
+firstPage.controller("mainCtrl",function($scope,$resource,$location){
+
   //initialization the login user model to nil
   $scope.logUser = {};
-  $scope.loginForm=true;
-
-  $scope.loginUsr = function()
-  {
+  
+  // invoking the front-api responsible for login scenario
+  $scope.loginUsr = function(){
     var logRes = $resource('/login');
+    logRes.save({
+      // getting the credentials from the login form
+      "mail_id":$scope.logUser.email, 
+      "password":$scope.logUser.password
+    },
+    // callback funtion from login api 
+    function(state){
+      if (state.msg != null){
+        // error message in wrong invalid credentials
+        $scope.logStatus=state.msg;
+        $scope.logFailMsg = true;
+        $scope.logUser = {};
+      }
+      else{
+        $scope.logFailMsg = false;
+        // redirecting to next page i.e home page on successful authentication
+        $location.path('/home')
+      }
+    });
+  }
+});       
 
-    logRes.save(
-                {
-                    "mail_id":$scope.logUser.email, 
-                    "password":$scope.logUser.password
-                },
+//registration form controller
+firstPage.controller("RegFormCtrl", function($scope,$resource){
 
-                function(state)
-                {
-                  if (state.msg != null)    
-                  {
-                    $scope.logStatus=state.msg;
-                    $scope.logFailMsg = true;
+  //initialization the registration user model to nil
+  $scope.regUser = {};
   
-                  }
-                  else
-                  {
-                    $scope.logFailMsg = false;
-                  
-                    $scope.loginForm=false;
-                  
-                    $scope.homePage="/home";
-                    // $(document).ready(function()
-                    //          {
-                    //             $scope.dat.datepicker({ minDate: -20, maxDate: "+2M"});
-                    //          }
-                    //         );
-                    $scope.logoutFlag = true;
-                  }
-                  $scope.logUser = {};
-                }                  
-               );
-  }                
+  //keeps sucess message hidden
+  $scope.regSucessMsg = false;
+  // invoking the front-api responsible for registration scenario
+  $scope.registerUsr = function(){
+    var regRes = $resource('/register');
+    regRes.save({
+      // getting the credentials from the registration form
+      "first_name":$scope.regUser.fName, 
+      "last_name":$scope.regUser.lName, 
+      "mail_id":$scope.regUser.email, 
+      "password":$scope.regUser.password
+    },
+    // callback funtion from registration api 
+    function(state){
+      $scope.regStatus=state.msg;    
+      $scope.regSucessMsg = true;
+      $scope.regUser = {};
+    });
+  }
+});
+
+// home-page controller
+firstPage.controller("homeCtrl", function($scope){
+  //initialization the login user model to nil
+  $scope.parcel = {};
   
-  $scope.packCollect =  function(prcl)
-  {
+  $scope.packCollect = function(prcl){
+    // some message after parcel is set
     $scope.confirmStatus="Thank you, The parcel will be collected on "+ prcl.dat+" from "+prcl.addrss+" by "+prcl.tim;
     $scope.confirmMsg=true; 
 
@@ -55,36 +83,5 @@ function LoginFormCtrl($scope,$resource)
     $scope.nextParcel="Schedule another parcel";
     $scope.anotherParcelMsg=true;
   }
-}
+});
 
-//registration form controller
-function RegFormCtrl($scope,$resource) 
-{
-
-  //initialization the registration user model to nil
-  $scope.regUser = {};
-
-  //keeps sucess message hidden
-  $scope.regSucessMsg = false;
-  
-  $scope.registerUsr = function()
-  {
-    var regRes = $resource('/register');
-
-    regRes.save(
-                {
-                    "first_name":$scope.regUser.fName, 
-                    "last_name":$scope.regUser.lName, 
-                    "mail_id":$scope.regUser.email, 
-                    "password":$scope.regUser.password
-                },
-
-                function(state)
-                {
-                  $scope.regStatus=state.msg;    
-                  $scope.regSucessMsg = true;
-                  $scope.regUser = {};
-                }
-               );
-  }
-}
